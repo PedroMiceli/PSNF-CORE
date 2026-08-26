4b8fd6e46c248bc24c2a44519461250a7b5eb5ca
PedroMiceli <pedro.miceli45@gmail.com>
2026-08-26 17:42:02 -0300

    ajuste

---
+++ psnf-api/src/main/java/com/psnf/api/psnf/PsnfApplication.java
@@ -0,0 +1,13 @@
+package com.psnf.api.psnf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PsnfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PsnfApplication.class, args);
+	}
+
+}
